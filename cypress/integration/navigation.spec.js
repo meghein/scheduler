9453f310332704c5d0eef538216e72f3569a5150
Meghan Hein <meghan.hein@gmail.com>
2020-07-15 14:27:03 -0400

    add cypress testing for appointments feature

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,10 +1,12 @@
 describe("Navigation", () => {
-  it("should visit root", () => {
-    cy.visit("/");
-  });
-  it("should navigate to Tuesday", () => {
-    cy.visit("/");
+  beforeEach(() => {
+    cy.visit("/")
+  })
   
+  it("should visit root", () => {
+  });
+
+  it("should navigate to Tuesday", () => {
     cy.contains("[data-testid=day]", "Tuesday")
       .click()
       .should("have.class", "day-list__item--selected")
