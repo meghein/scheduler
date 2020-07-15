8f42cff609a637d15bc562eb555f6a542c88e2e6
Meghan Hein <meghan.hein@gmail.com>
2020-07-15 13:40:25 -0400

    add first cypress test for navigation

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -7,7 +7,7 @@ export default function DayList(props) {
       key={day.id}
       name={day.name}
       spots={day.spots}
-      selected={day.name === props.days}
+      selected={day.name === props.day}
       setDay={props.setDay}
     />
   })
